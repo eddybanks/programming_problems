5e0b7ebb999719e817f1f624a3cb125f50178e58
Edwin Agbenyega <eddy.devv@gmail.com>
2019-09-27 01:25:28 -0600

    Completed gofish python game

--- games/gofish.py
+++ games/gofish.py
@@ -1,12 +1,51 @@
 import math, random
+# deque is converts a list into a queue
 from collections import deque
 
+# this function removes 7 cards from the deck and gives it to the player
 def serve(deck):
   hand = []
+  # remove card from deck using popleft function and append it to hand 7 times - range(7) for loop
   for _ in range(7):
     hand.append(deck.popleft())
   
   return hand
+
+# this function represents each round of the game
+def play_round(player, player2):
+  # if the player is you, print your hand and collection
+  if player.turn == 'p':
+    player.print()
+
+  # if player doesn't have any cards in hand, check to see if there are cards in the deck to draw from
+  if(len(player.hand) == 0):
+    # if the deck isn't empty, draw from the deck... else print "You are out of cards"
+    if(len(deck) > 0):
+      print("Player has no cards in hand and needs to draw")
+      player.draw(deck)
+    else:
+      print("You are out of cards")
+      print()
+  # if player has cards in hand, ask other opponent for a desired card within the player's hand
+  else:
+    demand = player.ask()
+    # if the card asked for is in the opponent's hand, the oponent loses the card to the player
+    if(demand in player2.hand):
+      player2.hand.remove(demand)
+      if player.turn == 'p':
+        print("The computer has a", demand, "and gives it to you")
+      else:
+        print("You have a", demand, "and give it to the computer")
+      # player gains the card and it is added to his hand
+      player.add_card(demand)
+      print()
+      # the player gets to ask again in a repeated round
+      play_round(player, player2)
+    # if the card asked for is not in the opponent's hand, the player has to draw from the deck
+    else:
+      print("Go fish!")
+      player.draw(deck)
+      print()
 
 
 class Player:
@@ -14,14 +53,19 @@ class Player:
     self.hand = []
     self.collection = []
     self.turn = turn
-    self.dictionary = {x:hand.count(x) for x in hand}
-    self.check_collection()
-
-  def check_collection(self):
-    if(self.turn == 'p'):
-      print("You have a pair of", k)
+    for i in hand:
+      self.add_card(i)
+    
+  def add_card(self, card):
+    if card in self.hand:
+      self.collection.append(card)
+      self.hand.remove(card)
+      if self.turn == 'p':
+        print("You have a pair of", card)
+      else:
+        print("The computer has a pair of", card)
     else:
-      print("The computer has a pair of", k)
+      self.hand.append(card)
 
   def draw(self, deck):
     drawn = deck.popleft()
@@ -32,19 +76,15 @@ class Player:
     
     self.add_card(drawn)
 
-  def add_card(card):
-    if card in self.hand:
-      self.collection.append(card)
-      if self.turn == 'p':
-        print("You have a pair of", card)
-    else:
-      self.hand.append(card)
-
   def ask(self):
     demand = ''
     if self.turn == 'p':
       demand = input("Pick a card to ask for: ")
-      print("You ask the computer for a", demand)
+      if(demand not in self.hand):
+        print("Sorry, you don't have any", demand, "in your hand")
+        self.ask()
+      else:
+        print("You ask the computer for a", demand)
     else:
       random.shuffle(self.hand)
       demand = self.hand[0]
@@ -52,9 +92,26 @@ class Player:
     
     return demand
 
+  def calculate(self):
+    result = len(self.collection)
+    if player.turn == 'p':
+      print("Your score is", result)
+    else:
+      print("The computer's score is", result)
+    
+    return result
+
   def print(self):
-    print("Your hand is: ", *self.hand)
-    print("Your collection is: ", *self.collection)
+    if self.turn == 'p':
+      if(len(self.hand) > 0):
+        print("Your hand is: ", *self.hand) 
+      if(len(self.collection) > 0):
+        print("Your collection is: ", *self.collection)
+    else:
+      if(len(self.hand) > 0):
+        print("The computer's hand is", *self.hand)
+      if(len(self.collection) > 0):
+        print("The computer's collection is: ", *self.collection)
 
 # Dictionary of Cards and their values 
 cards = {'goldfish': 1, 'catfish': 1, 'trout': 1, 'grouper': 1, 'tuna': 2, 'salmon': 2, 'sturgeon': 2, 'piranha': 3, 'swordfish': 4, 'clownfish': -1 }
@@ -69,12 +126,23 @@ player_hand = serve(deck)
 computer_hand = serve(deck)
 p = Player(player_hand, 'p')
 c = Player(computer_hand, 'c')
+print()
+player = p
+player2 = c
 
-# while(len(deck) > 0):
+while(len(deck) > 0):
+  play_round(player, player2)
+  tmp = player
+  player = player2
+  player2 = tmp
+
+
 p.print()
-demand = p.ask()
-if(demand in c.hand | demand in c.collection):
-  print("The computer has a", demand, "and gives it to you")
-  p.add_card(demand)
-
-
+p1 = p.calculate()
+c.print()
+p2 = c.calculate()
+if(p1 > p2):
+  winner ="you"
+else:
+  winner = "the computer"
+print("The winner is ", winner)
